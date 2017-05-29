1d1e26000a70926d81d9562454f3fa863e984385
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-05-28 18:17:24 -0700

    added while_you_wait.js assignment for a birthday countdown demo' git status

---
+++ while_you_wait.js
@@ -0,0 +1,16 @@
+function whileYouWait(daysUntilMyBirthday){
+  if (daysUntilMyBirthday > 30) {
+    console.log(daysUntilMyBirthday + " whole freaking days until my birthday. Nnnnnnnnnnnnnnnnnngh.");
+  }
+  else if (daysUntilMyBirthday > 4) {
+    console.log(daysUntilMyBirthday + " more days--not much longer!");
+  }
+  else if (daysUntilMyBirthday > 0) {
+    console.log("ONLY " + daysUntilMyBirthday + " DAYS TILL MY BIRTHDAY!!!");
+  }
+  else {
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+    console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+    console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
+  }
+}
