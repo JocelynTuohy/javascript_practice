78f0fe52ccf7492d68c5499300c80e5773227699
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-05-28 17:26:50 -0700

    original commit: first two javascript assignments

---
+++ i_do_declare.js
@@ -0,0 +1,27 @@
+// declare the following variable types:
+// four numbers
+var num1 = 89359;
+var num2 = -89.5;
+var num3 = 78/43;
+var num4 = 0.000004;
+// three strings
+var string1 = ""
+var string2 = "I saw something nasty in the woodshed."
+var string3 = "I have got this feeling/deep inside of me/girl, you just don't realize what you do to me/when you hold me/in your arms so tight/you let me know/everything's alright/IIIIIIIIIIIIIIIIIIII, I'm hooked on a feeling/I'm high on believing/that you're in love with meeeeeeeeeeeeeeeeeeeeeeeee."
+// two booleans
+var boolean1 = true;
+var boolean2 = false;
+// one undefined
+var undies;
+
+// display all variables on console with description
+console.log("The first number is "+num1);
+console.log("The second number is "+num2);
+console.log("The third number is "+num3);
+console.log("The fourth number is "+num4);
+console.log("The first string is "+string1);
+console.log("The second string is "+string2);
+console.log("The third string is "+string3);
+console.log("The first boolean is "+boolean1);
+console.log("The second boolean is "+boolean2);
+console.log("The undefined variable is "+undies);
